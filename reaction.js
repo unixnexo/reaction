0d80b09297a31f66170d77f79428d5f9e9cff8e5
unixnexo <unixnexo@gmail.com>
2024-08-10 17:53:14 +0330

    dev

--- reaction.js
+++ reaction.js
@@ -31,22 +31,14 @@ function createRipple(el, X, Y) {
 
 const rippleElements = document.querySelectorAll('.ripple-reaction-effect');
 if (rippleElements.length !== 0) {
-  const userPreferenceHoverColor = document.body.getAttribute('data-reaction-hover') || 'rgba(128, 128, 128, 0.111)';
   rippleElements.forEach(el => {
 
-    const initialBgColor = window.getComputedStyle(el).backgroundColor;
     el.style.transition = 'background 0.2s ease-in-out';
     el.style.overflow = 'hidden';
     el.style.position = 'relative';
     el.style.display = 'inline-block';
 
     // mouse
-    el.addEventListener('mouseover', () => {
-      el.style.backgroundColor = userPreferenceHoverColor;
-    });
-    el.addEventListener('mouseleave', () => {
-      el.style.backgroundColor = initialBgColor;
-    });
     el.addEventListener('click', (e) => {
       createRipple(el, e.clientX, e.clientY);
     });
@@ -66,16 +58,15 @@ if (rippleElements.length !== 0) {
 // push
 const pushedElements = document.querySelectorAll('.push-reaction-effect');
 if (pushedElements.length !== 0) {
-  const userPreferenceHoverColor = document.body.getAttribute('data-reaction-hover') || 'rgba(128, 128, 128, 0.111)';
   const downScale = document.body.getAttribute('data-reaction-startPush') || '0.9';
   const upScale = document.body.getAttribute('data-reaction-endPush') || '1';
   const startScale = `scale(${downScale})`;
   const endScale = `scale(${upScale})`;
 
   pushedElements.forEach(el => {
-    const initialBgColor = window.getComputedStyle(el).backgroundColor;
     el.style.willChange = 'transform';
     el.style.transition = 'transform .1s ease-in-out';
+    el.style.transition = 'background .2s ease-in-out';
 
     const presStart = () => {
       el.style.transform = startScale;
@@ -87,12 +78,6 @@ if (pushedElements.length !== 0) {
     // mouse
     el.addEventListener('mousedown', presStart);
     el.addEventListener('mouseup', presEnd)
-    el.addEventListener('mouseover', () => {
-      el.style.backgroundColor = userPreferenceHoverColor;
-    });
-    el.addEventListener('mouseleave', () => {
-      el.style.backgroundColor = initialBgColor;
-    });
 
     // touch
     el.addEventListener('touchstart', () => {
@@ -107,6 +92,7 @@ if (pushedElements.length !== 0) {
 
 
 // bg change
+let mouseOver = false;
 const coloredElements = document.querySelectorAll('.color-reaction-effect');
 if (coloredElements.length !== 0) {
   const userPreferenceBgColor = document.body.getAttribute('data-reaction-bg-color') || 'rgba(128, 128, 128, 0.533)';
@@ -114,44 +100,51 @@ if (coloredElements.length !== 0) {
   
   coloredElements.forEach(el => {
     const initialBgColor = window.getComputedStyle(el).backgroundColor;
-    let isMouseDown = false;
-    let isMouseOver = false;
-
     el.style.transition = 'background 0.2s ease-in-out';
 
-    const initBg = () => {
-      if (!isMouseDown) {
-        el.style.backgroundColor = initialBgColor;
-      }
-    };
-
-    el.addEventListener('mouseover', () => {
-      isMouseOver = true;
-      if (!isMouseDown) {
-        el.style.backgroundColor = userPreferenceHoverColor;
-      }
-    });
-
-    el.addEventListener('mouseleave', () => {
-      isMouseOver = false;
-      initBg();
-    });
-
     el.addEventListener('mousedown', () => {
-      isMouseDown = true;
       el.style.backgroundColor = userPreferenceBgColor;
     });
 
     el.addEventListener('mouseup', () => {
       setTimeout(() => {
-        isMouseDown = false;
-        if (!isMouseOver) {
-          initBg();
+        if (!mouseOver) {
+          el.style.backgroundColor = initialBgColor;
         } else {
           el.style.backgroundColor = userPreferenceHoverColor;
         }
       }, 150);
+      
     });
   });
+}
+
+
+// enable hover
+if (document.body.hasAttribute('enable-hover-reaction')) {
+  function hoverEffect(el) {
+    const userPreferenceHoverColor = document.body.getAttribute('data-reaction-hover') || 'rgba(128, 128, 128, 0.111)';
+    const initialBgColor = window.getComputedStyle(el).backgroundColor;
+
+    el.addEventListener('mouseover', () => {
+      el.style.backgroundColor = userPreferenceHoverColor;
+      mouseOver = true;
+    });
+    el.addEventListener('mouseleave', () => {
+      el.style.backgroundColor = initialBgColor;
+      mouseOver = false;
+    });
+  }
+
+  rippleElements.forEach(el => {
+    hoverEffect(el);
+  });
+  pushedElements.forEach(el => {
+    hoverEffect(el);
+  });
+  coloredElements.forEach(el => {
+    hoverEffect(el);
+  });
+
 }
 
