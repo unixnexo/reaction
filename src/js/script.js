ab5fbd790ef13666056eda128b748d22495650f3
unixnexo <unixnexo@gmail.com>
2024-08-27 18:59:13 +0330

    dev

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,46 @@
+// const navBtns = document.querySelectorAll('#nav button');
+// navBtns.forEach(btn => {
+//     btn.addEventListener('click', () => {
+//         // hide all sections
+//         const allSections = document.querySelectorAll('.section');
+//         allSections.forEach(sec => {
+//             sec.classList.add('hidden');
+//         });
+//         // show associated section
+//         const section = document.querySelector(`#${btn.textContent}`);        
+//         if (section !== null) {
+//             section.classList.remove('hidden');
+//         }
+//     });
+// });
+
+
+
+
+///////// test 
+document.querySelector('#select-effect-form').addEventListener('change', () => {
+    const selectedOption = document.querySelector('#select-effect-form select').value;
+
+    const demoBtns = document.querySelectorAll('#demo-btns-wrapper > *');
+
+    demoBtns.forEach(btn => {
+
+        // remove already existed effect
+        btn.classList.forEach(clsName => {
+            if (clsName.includes('-reaction-effect')) {
+                btn.classList.remove(clsName);
+            }
+        });
+
+        // add new effect
+        btn.classList.add(`${selectedOption}-reaction-effect`);
+    });
+
+    reaction();
+});
+
+
+
 const navBtns = document.querySelectorAll('#nav button');
 navBtns.forEach(btn => {
     btn.addEventListener('click', () => {
@@ -16,25 +59,25 @@ navBtns.forEach(btn => {
 
 
 
+function router() {
 
-///////// test 
-document.querySelector('#select-effect-form').addEventListener('change', () => {
-  const selectedOption = document.querySelector('#select-effect-form select').value;
+  function removeSections(thisSec) {
+    document.querySelectorAll('.section').forEach(sec => {
+      if (sec.id !== thisSec) {
+        sec.remove();
+      }
+    });
+  }
 
-  const demoBtns = document.querySelectorAll('#demo-btns-wrapper > *');
+  const home = document.getElementById('home');
+  const demo = document.getElementById('demo');
 
-  demoBtns.forEach(btn => {
-    // Reset the element (remove event listeners and inline styles)
-    resetElement(btn);
-
-    // Add new effect class
-    btn.classList.add(`${selectedOption}-reaction-effect`);
-  });
-
-  // Reapply effects
-  reaction();
-});
-
-
-
-
+  if (window.location.href.endsWith('?home') || window.location.href.endsWith('?home/') || window.location.href.endsWith('')) {
+    removeSections('home');
+    console.log('home');
+  } else if (window.location.href.endsWith('?demo') || window.location.href.endsWith('?demo/')) {
+    removeSections('demo');
+    console.log('demo');
+  }
+}
+window.addEventListener('load', () => router());
