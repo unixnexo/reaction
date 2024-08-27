8d07d441653a133b1f1132da22d5a30a4ffcb308
unixnexo <unixnexo@gmail.com>
2024-08-27 20:02:28 +0330

    dev

--- src/js/script.js
+++ src/js/script.js
@@ -1,83 +1,65 @@
-// const navBtns = document.querySelectorAll('#nav button');
-// navBtns.forEach(btn => {
-//     btn.addEventListener('click', () => {
-//         // hide all sections
-//         const allSections = document.querySelectorAll('.section');
-//         allSections.forEach(sec => {
-//             sec.classList.add('hidden');
-//         });
-//         // show associated section
-//         const section = document.querySelector(`#${btn.textContent}`);        
-//         if (section !== null) {
-//             section.classList.remove('hidden');
-//         }
-//     });
-// });
+/**
+ * section state
+ */
+const main = document.getElementById('main');
+const home = document.getElementById('home');
+const demo = document.getElementById('demo');
+
+function router() {
+  // remove all sections
+  function removeSections() {
+    document.querySelectorAll('.section').forEach(sec => {
+      sec.remove();
+    });
+  }
+
+  // check which section should be up
+  if (window.location.href.endsWith('?home') || window.location.href.endsWith('?home/')) {
+    removeSections();
+    main.insertBefore(home, main.firstChild);
+  } else if (window.location.href.endsWith('?demo') || window.location.href.endsWith('?demo/')) {
+    removeSections();
+    main.insertBefore(demo, main.firstChild);
+  }
+}
+window.addEventListener('load', () => router());
+
+// change state to show specific section
+const navBtns = document.querySelectorAll('#nav button');
+navBtns.forEach(btn => {
+  btn.addEventListener('click', () => {
+    const newUrl = `${window.location.pathname}?${btn.textContent}/`;
+    window.history.pushState({}, '', newUrl);
+    router();
+  });
+});
 
 
 
 
 ///////// test 
-document.querySelector('#select-effect-form').addEventListener('change', () => {
-    const selectedOption = document.querySelector('#select-effect-form select').value;
+// document.querySelector('#select-effect-form').addEventListener('change', () => {
+//     const selectedOption = document.querySelector('#select-effect-form select').value;
 
-    const demoBtns = document.querySelectorAll('#demo-btns-wrapper > *');
+//     const demoBtns = document.querySelectorAll('#demo-btns-wrapper > *');
 
-    demoBtns.forEach(btn => {
+//     demoBtns.forEach(btn => {
 
-        // remove already existed effect
-        btn.classList.forEach(clsName => {
-            if (clsName.includes('-reaction-effect')) {
-                btn.classList.remove(clsName);
-            }
-        });
+//         // remove already existed effect
+//         btn.classList.forEach(clsName => {
+//             if (clsName.includes('-reaction-effect')) {
+//                 btn.classList.remove(clsName);
+//             }
+//         });
 
-        // add new effect
-        btn.classList.add(`${selectedOption}-reaction-effect`);
-    });
+//         // add new effect
+//         btn.classList.add(`${selectedOption}-reaction-effect`);
+//     });
 
-    reaction();
-});
-
-
-
-const navBtns = document.querySelectorAll('#nav button');
-navBtns.forEach(btn => {
-    btn.addEventListener('click', () => {
-        // hide all sections
-        const allSections = document.querySelectorAll('.section');
-        allSections.forEach(sec => {
-            sec.classList.add('hidden');
-        });
-        // show associated section
-        const section = document.querySelector(`#${btn.textContent}`);        
-        if (section !== null) {
-            section.classList.remove('hidden');
-        }
-    });
-});
+//     reaction();
+// });
 
 
 
-function router() {
 
-  function removeSections(thisSec) {
-    document.querySelectorAll('.section').forEach(sec => {
-      if (sec.id !== thisSec) {
-        sec.remove();
-      }
-    });
-  }
 
-  const home = document.getElementById('home');
-  const demo = document.getElementById('demo');
-
-  if (window.location.href.endsWith('?home') || window.location.href.endsWith('?home/') || window.location.href.endsWith('')) {
-    removeSections('home');
-    console.log('home');
-  } else if (window.location.href.endsWith('?demo') || window.location.href.endsWith('?demo/')) {
-    removeSections('demo');
-    console.log('demo');
-  }
-}
-window.addEventListener('load', () => router());
